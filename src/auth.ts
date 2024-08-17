ee793c1358e0a030abb0954ee2f829e3fcbb66f9
Mihir2423 <mihirraj444@gmail.com>
2024-08-17 13:28:36 +0530

    update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -2,11 +2,19 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/db"
 import Google from "next-auth/providers/google"
-import Resend from "next-auth/providers/resend"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google, Resend({
-    from :"no-reply@mihir.test.com"
-  })],
+  providers: [Google],
+  callbacks: {
+    async session({ session, user }) {
+      if (user) {
+        session.user = user
+      }
+      return session;
+    }
+  },
+  session: {
+    strategy: "jwt",
+  }
 })
