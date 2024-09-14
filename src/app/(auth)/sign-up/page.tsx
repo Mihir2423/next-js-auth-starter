9236980e638a159336330e01594573b1dffe5e37
Mihir2423 <mihirraj444@gmail.com>
2024-09-14 12:46:15 +0530

    feat: complete sign up page ui

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,103 @@
-import { AuthLayout } from '@/components/auth/layout'
-import React from 'react'
+"use client";
 
-type Props = {}
+import { AuthLayout } from "@/components/auth/layout";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const registrationSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    passwordConfirmation: z.string().min(8),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords don't match",
+    path: ["passwordConfirmation"],
+  });
+
+type Props = {};
 
 const SignUpPage = (props: Props) => {
+  const form = useForm<z.infer<typeof registrationSchema>>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof registrationSchema>) {
+    // execute(values);
+    console.log(values);
+  }
   return (
-    <AuthLayout type="Sign-Up" text="Create an account to get started">SignUpPage</AuthLayout>
-  )
-}
+    <AuthLayout type="Sign-Up" text="Create an account to get started">
+      <div className="w-full">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="">Email address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="">Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="passwordConfirmation"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="">Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Confirm Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full py-[4px] text-sm">
+              Continue
+              {/* {isPending && <Loader2 className="w-4 h-4 ml-2 animate-spin" />} */}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </AuthLayout>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
